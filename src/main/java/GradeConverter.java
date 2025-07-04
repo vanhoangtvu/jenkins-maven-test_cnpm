bce83228d024ee41816495e6e955c851e760ec5a
Nguyen Van Hoang <160744592+vanhoangtvu@users.noreply.github.com>
2025-07-04 16:09:46 +0700

    Update GradeConverter.java

--- src/main/java/GradeConverter.java
+++ src/main/java/GradeConverter.java
@@ -1,6 +1,7 @@
 public class GradeConverter {
     public String convert(int score)
     {
+        
         if(score <= 100 && score >= 90) {
             return "A";
         } else if(score <= 89 && score >= 80) {
