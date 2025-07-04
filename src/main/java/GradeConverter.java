6f68dd4ca01aee01910eadc47c5611f2a3f802ae
Nguyen Van Hoang <160744592+vanhoangtvu@users.noreply.github.com>
2025-07-04 16:10:04 +0700

    Update GradeConverter.java
    fix error

--- src/main/java/GradeConverter.java
+++ src/main/java/GradeConverter.java
@@ -1,7 +1,6 @@
 public class GradeConverter {
     public String convert(int score)
     {
-        
         if(score <= 100 && score >= 90) {
             return "A";
         } else if(score <= 89 && score >= 80) {
